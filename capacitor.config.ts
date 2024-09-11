f3dd674743f0885c7a98dd36f843302785a97aed
LarissaGiovanna <lgiova544@gmail.com>
2024-09-11 15:08:56 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SafeCity',
+  webDir: 'www'
+};
+
+export default config;
